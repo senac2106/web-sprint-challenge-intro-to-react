b41cfda58a473e3af72747f4b202691815c0908c
¨senac2106 <emiliosneac7@gmail.com>
2021-05-14 11:54:16 -0700

    first try, i need for each

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from "axios"
+import Character from "./components/Character"
 
+
+const url = `https://swapi.dev/api/people`;
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -8,10 +12,26 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [characterList, setCharacterList] = useState([]);
+  useEffect(() => {
+    axios.get(url)
+    .then(({data}) => {
+      setCharacterList(data);
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }, [])
+
+  characterList.forEach((Element) =>{
+  
+   console.log(Element)
+  })
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character props={characterList} />
     </div>
   );
 }
